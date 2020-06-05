0e659aac57d665c6354e60b97f244334d3540121
larsverp2803@gmail.com <larsverp2803@gmail.com>
2020-06-05 13:47:22 +0200

    Special request

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,7 +9,7 @@
         </div>
         <div class="row articles">
             @foreach($products as $product)
-            <div class="col-sm-6 col-md-4 item"><a href="#"><img class="img-fluid" style="width:300px;height:260px;object-fit:cover;" src="{{$product->thumbnail}}"></a>
+            <div class="col-sm-6 col-md-4 item"><a href="/product/{{$product->id}}"><img class="img-fluid" style="width:300px;height:260px;object-fit:cover;" src="{{$product->thumbnail}}"></a>
                 <h3 class="name">{{$product->name}} - €{{$product->price}}</h3>
                 <p class="description">{{ substr($product->description, 0,  250)."..." }}</p><a class="action" href="/product/{{$product->id}}"><i class="fa fa-arrow-circle-right"></i></a>
             </div>
